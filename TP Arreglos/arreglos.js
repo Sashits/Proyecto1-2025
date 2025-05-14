428f6bf82cb82f348788b4665ad0738060775627
alumno <alumno@Instituto.local>
2025-05-14 08:23:26 -0300

    arreglos js

---
+++ TP Arreglos/arreglos.js
@@ -0,0 +1,22 @@
+let colores = ['Blanco', 'Negro', 'Azul', 'Violeta', 'Rojo' ] ;
+console.log(colores) ; 
+
+console.log(colores[0]);
+console.log(colores[colores.length - 1]);
+
+console.log(colores.length) ;
+
+colores.push('Rosa') ;
+console.log(colores) ; 
+
+colores.unshift('Amarillo');
+console.log(colores) ; 
+
+colores.pop(colores);
+console.log(colores) ; 
+
+colores.shift(colores);
+console.log(colores) ; 
+
+
+
