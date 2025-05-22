157a4729d3828c241320e261f3a32328b98146e8
alumno <alumno@Instituto.local>
2025-05-22 11:17:57 -0300

    tp2

---
+++ TP2 Arreglos/script.js
@@ -0,0 +1,38 @@
+//Ejercicio 1
+//1
+let frutas = ['Manzana', 'Banana', 'Naranja'];
+console.log(frutas)
+//2
+frutas.push('Sandia');
+console.log(frutas);
+
+//3
+frutas.unshift('Kiwi');
+console.log(frutas);
+
+//4
+frutas.pop();
+console.log(frutas);
+
+//5
+frutas.shift();
+console.log(frutas);
+
+//6
+console.log(frutas);
+
+//Ejercicio 2
+//1
+let nombres = ['Martina','María','Lourdes'];
+//2
+console.log(nombres.length);
+
+//3 y 4 
+nombres.push('David', 'Sandra');
+console.log(nombres);
+
+//Ejercicio 3
+let numeros = ['22', '13', '24'];
+
+for (let i = 0; i < numeros.length; i++) 
+console.log (numeros[i]);
