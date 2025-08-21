958425940fe7b78fea48be1d626fcb2e1c2540d6
alumno <alumno@Instituto.local>
2025-08-21 11:18:32 -0300

    pokemon

---
+++ Poke api/poke.js
@@ -0,0 +1,19 @@
+let titulo = document.querySelector('#titulo')
+let imagen = document.querySelector('#imagen')
+let boton = document.querySelector('#boton')
+
+boton.onclick = function () {
+
+    fetch('https://pokeapi.co/api/v2/pokemon-form/157/')
+
+
+.then(respuesta => respuesta.json())
+
+.then(datos => {
+    console.log(datos)
+
+    titulo.textContent = datos.name
+    imagen.src = datos.srpites.front_shiny
+})
+
+}
